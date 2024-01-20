43349af976ccde67201180750aa8274e095a9b64
yongruifang <yongruifang@163.com>
2024-01-20 14:51:36 +0800

    feat(parse tree): parse tree, the first step to parse sql
    next step to do, need to visit tree

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,15 +1,35 @@
 package com.example;
 
 import java.io.*;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import com.example.antlr4.SqlBaseLexer;
+import com.example.antlr4.SqlBaseParser;
+import com.example.parser.AstVisitor;
+import com.example.tree.Node;
 
 class App {
     public static void main(String[] args) throws IOException {
-        System.out.print(">> "); // 打印提示符
-        InputStream input = System.in; // 标准输入流
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-        String sql = reader.readLine(); // 读取一行输入
-        // @TODO: 解析输入的sql语句
-        System.out.println("解析sql语句: " + sql);
+        /*
+         * System.out.print(">> "); // 打印提示符
+         * InputStream input = System.in; // 标准输入流
+         * BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+         * String sql = reader.readLine(); // 读取一行输入
+         */
+        String sql = "SELECT City, State FROM cities";
+        // 解析输入的sql语句
+        // 1. 生成语法树
+        SqlBaseLexer lexer = new SqlBaseLexer(CharStreams.fromString(sql));
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        SqlBaseParser parser = new SqlBaseParser(tokenStream);
+        ParserRuleContext tree = parser.singleStatement();
+        System.out.println("语法树: " + tree.toStringTree(parser));
+        // 2. @TODO: 遍历语法树，自定义访问者
+        AstVisitor eval = new AstVisitor();
+        Node node = eval.visit(tree);
+        System.out.println("返回: " + node);
         // @TODO: 执行sql语句
         System.out.println("执行sql语句: " + sql);
     }
---
+++ demo/src/main/java/com/example/parser/AstVisitor.java
@@ -0,0 +1,8 @@
+package com.example.parser;
+
+import com.example.antlr4.SqlBaseBaseVisitor;
+import com.example.tree.Node;
+
+public class AstVisitor extends SqlBaseBaseVisitor<Node> {
+
+}
